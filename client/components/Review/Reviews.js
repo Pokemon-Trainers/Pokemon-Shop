c31bb0e811cba19bf4ba39b68d76b118ec9b9b20
Sher iKwong <sherikwo@buffalo.edu>
2018-06-07 14:22:08 -0400

    Toggled add review

--- client/components/Review/Reviews.js
+++ client/components/Review/Reviews.js
@@ -4,30 +4,53 @@ import { connect } from "react-redux";
 import Review from "./Review";
 import AddReview from "./AddReview";
 
-const Reviews = props => {
-  const reviews = props.reviews;
+class Reviews extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toggleAddReview: false
+    }
+    this.handleToggle = this.handleToggle.bind(this);
+  }
 
-  if (reviews.length === 0) {
+  handleToggle(event) {
+    if (this.state.toggleAddReview === true) {
+      this.setState({
+        toggleAddReview: false
+      })
+    } else {
+      this.setState({
+        toggleAddReview: true
+      })
+    }
+    console.log(this.state.toggleAddReview);
+  }
+
+  render() {
+    const reviews = this.props.reviews;
+
+    if (reviews.length === 0) {
+      return (
+        <div>
+          <h2>Reviews</h2>
+          <hr />
+          <p>There are no reviews...</p>
+        </div>
+      );
+    }
+
     return (
       <div>
         <h2>Reviews</h2>
-        <hr />
-        <p>There are no reviews...</p>
+        {reviews.map((review, key) => (
+          <Review key={key} review={review} reviews={reviews} />
+        ))}
+        <button type="button" onClick={this.handleToggle}>Submit a Review</button>
+
+        {this.state.toggleAddReview && <AddReview pokemon={this.props.selectedPokemon} />}
       </div>
     );
   }
-
-  return (
-    <div>
-      <h2>Reviews</h2>
-      {reviews.map((review, key) => (
-        <Review key={key} review={review} reviews={reviews} />
-      ))}
-      <button type="button">Submit a Review</button>
-
-      <AddReview pokemon={props.selectedPokemon}/>
-    </div>
-  );
-};
+}
 
 export default Reviews;
