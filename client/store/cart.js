7eb190d8a4f372fa61c3e1175737be14d0b6312f
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2018-06-07 15:42:55 -0400

    can edit quantity and delete items in cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -5,10 +5,23 @@ import axios from "axios";
 const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 const CLEAR_CART = "CLEAR_CART";
 const SET_CART = "SET_CART";
+const ADD_TO_CART = "ADD_TO_CART";
 
 // Action creators
 export const setCart = (itemId, qty) => ({
   type: SET_CART,
+  itemId,
+  qty
+});
+
+export const removeFromCart = (itemId, qty) => ({
+  type: REMOVE_FROM_CART,
+  itemId,
+  qty
+});
+
+export const addToCart = (itemId, qty) => ({
+  type: ADD_TO_CART,
   itemId,
   qty
 });
@@ -21,7 +34,7 @@ const clearCart = () => ({
 // Reducer
 
 const cartReducer = (state = [], action) => {
-  console.log("state on cart", state);
+  console.log("state on qty", state);
   switch (action.type) {
     case SET_CART:
       console.log("We are setting cart", state, action);
@@ -29,13 +42,32 @@ const cartReducer = (state = [], action) => {
         return state
           .map(item => {
             if (action.itemId === item.itemId) {
-              return { ...item, qty: action.qty };
+              return { ...item, qty: item.qty + action.qty };
             }
             return { ...item };
           })
           .filter(item => item.qty !== 0);
       } else {
         return [...state, { itemId: action.itemId, qty: action.qty }];
+      }
+
+    case REMOVE_FROM_CART:
+      if (state.find(item => item.itemId === action.itemId)) {
+        return state.map(item => {
+          if (action.itemId === item.itemId) {
+            return { ...item, qty: item.qty - 1 };
+          }
+          return { ...item };
+        });
+      }
+    case ADD_TO_CART:
+      if (state.find(item => item.itemId === action.itemId)) {
+        return state.map(item => {
+          if (action.itemId === item.itemId) {
+            return { ...item, qty: item.qty + 1 };
+          }
+          return { ...item };
+        });
       }
 
     case CLEAR_CART:
