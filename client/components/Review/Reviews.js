9f5d7e4672e6ac2f3a8149221d84fef3badb7be5
Sher iKwong <sherikwo@buffalo.edu>
2018-06-07 23:00:29 -0400

    Worked on sidebar

--- client/components/Review/Reviews.js
+++ client/components/Review/Reviews.js
@@ -34,7 +34,7 @@ class Reviews extends React.Component {
           <h2>Reviews</h2>
           <hr />
           <p>There are no reviews...</p>
-          <button type="button" onClick={this.handleToggle}>Submit a Review</button>
+          <button className="btn btn-info" type="button" onClick={this.handleToggle}>Submit a Review</button>
           {this.state.toggleAddReview && <AddReview pokemon={this.props.selectedPokemon} />}
         </div>
       );
@@ -45,8 +45,8 @@ class Reviews extends React.Component {
         <h2>Reviews</h2>
         {reviews.map((review, key) => (
           <Review key={key} review={review} reviews={reviews} />
-        ))}
-        <button type="button" onClick={this.handleToggle}>Submit a Review</button>
+        ))}<br/>
+        <button type="button" className="btn btn-info" onClick={this.handleToggle}>Submit a Review</button>
 
         {this.state.toggleAddReview && <AddReview pokemon={this.props.selectedPokemon} />}
       </div>
