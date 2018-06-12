a7d63e587cd5658fd894e730f20fdc4b52ed9756
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2018-06-12 18:44:36 -0400

    edited styles

--- client/components/Admin/AllUsers.js
+++ client/components/Admin/AllUsers.js
@@ -39,33 +39,47 @@ class AllUsers extends React.Component {
 
   render() {
     return (
-      <div className="">
-        <h1>All Users</h1>
+      <div className="order container">
+        <h2 className="specs row mb-4">All Users</h2>
         {this.props.users &&
           this.props.users.map(user => (
-            <div key={user.id}>
+            <div className="" key={user.id}>
               {this.props.user.id !== user.id && (
-                <li>
-                  {user.email}
-                  <p>{user.admin ? "admin" : "not an Admin"}</p>
-                  <span>
-                    {!user.admin ? (
-                      <button
-                        type="button"
-                        onClick={event => this.handleSave(event, user)}
-                      >
-                        Make Admin
-                      </button>
-                    ) : (
-                      <button
-                        type="button"
-                        onClick={event => this.handleSave(event, user)}
-                      >
-                        Remove As Admin
-                      </button>
-                    )}
-                  </span>
-                </li>
+                <div className="row mb-4">
+                  <div className="col">
+                    <h4> {user.email}</h4>
+                  </div>
+                  <div className="col">
+                    <p className="notadmin">
+                      {user.admin ? (
+                        <img src="http://i67.tinypic.com/bhx5za.png" />
+                      ) : (
+                        "Not An Admin"
+                      )}
+                    </p>
+                  </div>
+                  <div className="col">
+                    <span>
+                      {!user.admin ? (
+                        <button
+                          className="adminbtn btn btn-info"
+                          type="button"
+                          onClick={event => this.handleSave(event, user)}
+                        >
+                          Make Admin
+                        </button>
+                      ) : (
+                        <button
+                          className="btn btn-danger"
+                          type="button"
+                          onClick={event => this.handleSave(event, user)}
+                        >
+                          Remove As Admin
+                        </button>
+                      )}
+                    </span>
+                  </div>
+                </div>
               )}
             </div>
           ))}
