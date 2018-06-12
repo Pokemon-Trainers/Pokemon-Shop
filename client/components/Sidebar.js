982b7b31fb685348a26f229d29e769127d361538
Linda Morales <lindagmorales94@gmail.com>
2018-06-12 15:10:08 -0400

    some styling

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Type from "./categories/Type";
-import Price from "./categories/Price";
-import Level from "./categories/Level";
+import React from 'react';
+import Type from './categories/Type';
+import Price from './categories/Price';
+import Level from './categories/Level';
 
 const Sidebar = props => {
   const {
@@ -12,46 +12,42 @@ const Sidebar = props => {
     toggleTypeHidden,
     typeHidden,
     priceHidden,
-    togglePriceHidden
+    togglePriceHidden,
   } = props;
 
   return (
     <div className="col-sm-5 col-md-3 d-none d-sm-block">
-      <div className="row ">
-        <div className="col-12">
-          <h3>Categories</h3>
-          <div className="nav-sidebar mx-auto">
-            <button
-              onClick={resetFilters}
-              className="btn btn-outline-primary mx-auto"
-            >
-              Reset Filters
-            </button>
-            <button
-              type="button"
-              className="btn btn-link"
-              id="Type"
-              onClick={toggleTypeHidden}
-            >
-              Type (17)
-            </button>
-            {!typeHidden && <Type handleTypeFilter={handleTypeFilter} />}
-
-            <button
-              type="button"
-              className="btn btn-link"
-              id="Type"
-              onClick={togglePriceHidden}
-            >
-              Price (10)
-            </button>
-
-            {!priceHidden && <Price handlePriceFilter={handlePriceFilter} />}
-
-            <Level />
-          </div>
-        </div>
+      <div className="text-center mt-4">
+        <h3>Categories</h3>
       </div>
+      <div className="mx-auto">
+        <button type="button" onClick={resetFilters} className="btn btn-info">
+          All
+        </button>
+      </div>
+      <div className="mx-auto">
+        <button
+          type="button"
+          className="btn btn-info text-center"
+          onClick={toggleTypeHidden}
+        >
+          Type (17)
+        </button>
+        {!typeHidden && <Type handleTypeFilter={handleTypeFilter} />}
+      </div>
+      <div className="mx-auto">
+        <button
+          type="button"
+          className="btn btn-info text-center"
+          onClick={togglePriceHidden}
+        >
+          Price (10)
+        </button>
+
+        {!priceHidden && <Price handlePriceFilter={handlePriceFilter} />}
+      </div>
+
+      <Level />
     </div>
   );
 };
